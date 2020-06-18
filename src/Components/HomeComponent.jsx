19dd54c7909dda106c36d41915442d82619ffa67
Shail <raval.shail1@gmail.com>
2020-06-18 14:04:47 +0530

    commit

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
 import Logo from '../Images/logo.png'
+import './styles.css'
 class HomeComponent extends Component{
     render(){
         return(
             <div>
+                <div className="avatar">
         <img src={Logo} alt = "Logo"/>
+        </div>
         <h1>WOLFPACK</h1>
         </div>
         )}
