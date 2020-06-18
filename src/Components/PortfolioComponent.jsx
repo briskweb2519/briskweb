89f3b8872df436f46f9e5118d8f07bbd3d0171a7
Shail <raval.shail1@gmail.com>
2020-06-18 23:28:19 +0530

    commit

--- src/Components/PortfolioComponent.jsx
+++ src/Components/PortfolioComponent.jsx
@@ -3,7 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Logo from '../Images/logo.png'
-
+import Speedwell1 from '../Images/speedwell/speedwell1.png'
+import Speedwell2 from '../Images/speedwell/speedwell2.png'
+import Speedwell3 from '../Images/speedwell/speedwell4.png'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -22,7 +24,7 @@ export default function ComplexGrid() {
   return (
     <div className={classes.root}>
       <div className="maincontainer">
-      <p style={{ fontSize: '40px', textAlign: 'left' }}><strong>Portfolio</strong></p>
+        <p style={{ fontSize: '40px', textAlign: 'left' }}><strong>Portfolio</strong></p>
       </div>
 
       <Paper className={classes.paper}>
@@ -30,7 +32,7 @@ export default function ComplexGrid() {
           <Grid item xs={12} md={6} style={{ textAlign: 'center', width: '100%', border: '1px solid' }}>
             <h1>Nutan Vastra Bhandar</h1>
           </Grid>
-          <Grid item xs={12} md={6} style={{width: '100%', border: '1px solid' }}>
+          <Grid item xs={12} md={6} style={{ width: '100%', border: '1px solid' }}>
             <div className="avatar">
               <img alt="complex" src={Logo} />
             </div>
@@ -39,14 +41,27 @@ export default function ComplexGrid() {
       </Paper>
 
       <Paper className={classes.paper}>
-        <Grid container spacing={2}>  
-          <Grid item xs={12} md={6} style={{width: '100%', border: '1px solid' }}>
-            <div className="avatar">
-              <img alt="complex" src={Logo} />
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6} style={{ width: '100%',border: '1px solid' }}>
+            
+            
+            <div className="trans">
+              <img alt="complex" src={Speedwell3} style={{ width: '40%', height: '200px' }} />
             </div>
+            <div className="trans" style={{position:'absolute',width:'30%'}}>
+              {/* <div className="avatar" style={{height:'40%'}}> */}
+                <img alt="complex" src={Speedwell1} style={{height:'200px', width:'200px'}}/>
+              {/* </div> */}
+            </div>
+            <div className="trans" style={{ width:'30%',position : 'absolute'}}>
+              {/* <div className="avatar" style={{height:'40'}}> */}
+                <img alt="complex" src={Speedwell2} style={{height:'200px', width:'200px'}}/>
+              {/* </div> */}
+            </div>
+            
           </Grid>
           <Grid item xs={12} md={6} style={{ textAlign: 'center', width: '100%', border: '1px solid' }}>
-            <h1>Nutan Vastra Bhandar</h1>
+            <h1>SPEEDWELL</h1>
           </Grid>
         </Grid>
       </Paper>
