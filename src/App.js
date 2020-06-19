709fdb07f116c4feccc432af1e97ea6d2ef00eaf
jack5972 <meetp6041@gmail.com>
2020-06-19 16:21:11 +0530

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomeComponent from './Components/HomeComponent';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router ,Route} from 'react-router-dom';
 import HeaderComponent from './Components/HeaderComponent';
 import Services from './Components/ServicesComponent'
 import AboutComponent from './Components/AboutComponent';
@@ -9,7 +9,7 @@ import PortfolioComponent from './Components/PortfolioComponent';
 import TechnologyComponent from './Components/TechnologyComponent';
 import ContactComponent from './Components/ContactComponent';
 import ScrollHandlerComponent from './Components/ScrollHandlerComponent';
-
+import SpeedwellComponent from './Components/SpeedwellComponent';
 
 function App() {
   return (
@@ -23,6 +23,7 @@ function App() {
         <TechnologyComponent />
         <AboutComponent />
         <ContactComponent />
+        <Route path="/speedwell" component={SpeedwellComponent}/>
       </Router>
     </div>
   )
--- src/Components/SpeedwellComponent.jsx
+++ src/Components/SpeedwellComponent.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Speedwell1 from '../Images/speedwell/speedwell1.png'
 import Speedwell2 from '../Images/speedwell/speedwell2.png'
 import Speedwell3 from '../Images/speedwell/speedwell4.png'
-import { Link } from 'react-router-dom';
+export default class ProjectDetailsComponent extends Component{
+    render(){
+        return(
+            <ComplexGrid/>
+        )
+    }
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -18,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ComplexGrid() {
+function ComplexGrid() {
   const classes = useStyles();
 
   return (
@@ -26,31 +33,29 @@ export default function ComplexGrid() {
       <div className={classes.root} className="maincontainer">
         <p style={{ fontSize: '40px', textAlign: 'left' }}><strong>Portfolio</strong></p>
       </div>
-      
-      <Link style={{textDecoration : 'none'}} to="/speedwell">
-        <Paper className={classes.paper}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={5} style={{paddingTop: '10%' }}>
-              <h1>SPEEDWELL</h1>
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <div className="trans">
-                <img alt="complex" src={Speedwell3} style={{ width: '45%', border: '1px solid' }} />
-              </div>
-              <div className="avatar">
-                <div className="trans">
-                  <img alt="complex" src={Speedwell2} style={{ width: '40%', border: '1px solid', translate: '135% -150%' }} />
-                </div>
-              </div>
-              <div className="avatar">
-                <div className="trans">
-                  <img alt="complex" src={Speedwell1} style={{ width: '40%', border: '1px solid', position: 'absolute', translate: '-2% -160%' }} />
-                </div>
-              </div>
-            </Grid>
+
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '10%' }}>
+            <h1>SPEEDWELL</h1>
           </Grid>
-        </Paper>
-      </Link>
+          <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '15%' }}>
+            <div className="trans">
+              <img alt="complex" src={Speedwell3} style={{ width: '45%', border: '1px solid' }} />
+            </div>
+            <div className="avatar">
+              <div className="trans">
+                <img alt="complex" src={Speedwell2} style={{ width: '40%', border: '1px solid', translate: '135% -150%' }} />
+              </div>
+            </div>
+            <div className="avatar">
+              <div className="trans">
+                <img alt="complex" src={Speedwell1} style={{ width: '40%', border: '1px solid', position: 'absolute', translate: '-2% -160%' }} />
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
 
       <Paper className={classes.paper}>
         <Grid container spacing={2}>
