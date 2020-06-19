f1cb9394eaf7f51c598e2f3e28ec1a51e3a4809c
jack5972 <meetp6041@gmail.com>
2020-06-19 19:00:24 +0530

    'commit'

--- src/Components/ContactComponent.jsx
+++ src/Components/ContactComponent.jsx
@@ -1,35 +1,60 @@
 import React, { Component } from 'react'
 import { TextField, Radio, Button, Grid, RadioGroup, FormControlLabel, FormControl, FormLabel, TextareaAutosize } from '@material-ui/core'
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import {Link} from 'react-router-dom'
-function FormControlLabelPlacement() {
+import { Link } from 'react-router-dom'
+function FormControlLabelPlacement(props) {
     return (
         <FormControl component="fieldset" style={{ width: '100%' }}>
             <FormLabel component="legend"><h5>Project</h5></FormLabel>
             <RadioGroup row aria-label="position" name="position" defaultValue="top" >
                 <Grid container>
                     <Grid item xs={12} md={4}>
-                        <FormControlLabel value="1" control={<Radio color="primary" />} label="Website" />
+                        <FormControlLabel name="project" value="website" control={<Radio color="primary" />} label="Website" onChange={props.onHandleChange} />
                     </Grid>
                     <Grid item xs={12} md={4}>
-                        <FormControlLabel value="2" control={<Radio color="primary" />} label="Web Application" />
+                        <FormControlLabel name="project" value="web-application" control={<Radio color="primary" />} label="Web Application" onChange={props.onHandleChange}/>
                     </Grid>
                     <Grid item xs={12} md={4}>
-                        <FormControlLabel value="3" control={<Radio color="primary" />} label="Other" />
+                        <FormControlLabel name="project" value="other" control={<Radio color="primary" />} label="Other" onChange={props.onHandleChange}/>
                     </Grid>
                 </Grid>
-
-
-
             </RadioGroup>
         </FormControl>
     );
 }
 
 export default class ContactComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: '', email: '', message: '',project : '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit() {
+        const templateId = 'template_S4FIQB8o';
+        this.sendFeedback(templateId, { name: this.state.name, email: this.state.email,project : this.state.project, message: this.state.message })
+    }
+
+    sendFeedback (templateId, variables) {
+        window.emailjs.send(
+          'wewolfpack', templateId,
+          variables
+          ).then(res => {
+            console.log('Email successfully sent!')
+          })
+          .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+      }
+
     render() {
         return (
-            <div style={{ textAlign: 'left'}} className="maincontainer" id="#contact">
+            <div style={{ textAlign: 'left' }} className="maincontainer" id="#contact">
                 <div >
                     <p style={{ fontSize: '40px', textAlign: 'left' }}><strong>Contact</strong></p>
                 </div>
@@ -38,16 +63,15 @@ export default class ContactComponent extends Component {
                     <form>
                         <Grid container spacing={2}>
                             <Grid item xs={12} md={6}>
-                                <TextField style={{ width: '80%', marginRight: '10%' }} id="standard-basic" label="Name" />
+                                <TextField style={{ width: '80%', marginRight: '10%' }} id="standard-basic" label="Name" name="name" value={this.state.name} onChange={this.handleChange} />
                             </Grid>
                             <Grid item xs={12} md={6}>
-                                <TextField style={{ width: '80%' }} id="standard-basic" label="E-Mail" />
+                                <TextField style={{ width: '80%' }} id="standard-basic" label="E-Mail" name="email" value={this.state.email} onChange={this.handleChange}/>
                             </Grid>
                         </Grid>
                     </form>
-
                     <div>
-                        <FormControlLabelPlacement />
+                        <FormControlLabelPlacement onHandleChange={this.handleChange}/>
                     </div>
 
                     <div>
@@ -56,8 +80,11 @@ export default class ContactComponent extends Component {
                             rows={10}
                             defaultValue=""
                             style={{ width: '90%' }}
+                            name="message"
+                            value={this.state.message}
+                            onChange={this.handleChange}
                         /><br />
-                        <Button style={{ marginTop: '2%' }} variant="contained" color="primary">
+                        <Button style={{ marginTop: '2%' }} variant="contained" color="primary" onClick={this.handleSubmit}>
                             Send
                         </Button>
                     </div>
@@ -66,8 +93,8 @@ export default class ContactComponent extends Component {
                     </div>
 
                 </div>
-                <div style={{textAlign:'center'}}>
-                <Link to="/home/#home"><ExpandLessIcon fontSize="large" style={{color:'black'}}/></Link>
+                <div style={{ textAlign: 'center' }}>
+                    <Link to="/home/#home"><ExpandLessIcon fontSize="large" style={{ color: 'black' }} /></Link>
                 </div>
             </div>
         )
