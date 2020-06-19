ad4954725aecc767fc804b7b578632f50767fa02
Shail <raval.shail1@gmail.com>
2020-06-19 15:10:27 +0530

    commit

---
+++ src/Components/DetailsComponent.jsx
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import './styles.css'
+import { Grid } from '@material-ui/core'
+import Solution from '../Images/Solution.png'
+import Meeting from '../Images/Meeting.png'
+import Pricing from '../Images/Pricing.png'
+class DetailsComponent extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data : [
+                {id : 1, img : Solution, name : "State of the Art Solutions", description : "It is 2020 and you should see that in the final product. This means much more than responsiveness and cross-browser-compatibility and involves best practices regarding performance, accessibility, security and usability.This results in appropriate design, secure websites, fast load times and happy customers. Ultimately, this is also the best and most reliable way to achieve Search Engine Optimization (SEO)."},
+                {id : 2, img : Meeting, name : "Non-Binding Introductory Meeting", description : "The first meeting or (video) call is always free and non-binding. It serves the purpose of defining the scope of the project and talking about possible solutions. If the expectations are not met in the end for whatever reason, that's unfortunate, but definitely not a problem."},
+                {id : 3, img : Pricing, name : "Transparent Pricing", description : "The price will be fully transparent at any stage of the project. That means: I will usually not charge on an hourly basis, but rather we will agree on a fixed price. Only if the scope of the project changes will the price be adjusted accordingly with prior agreement. Therefore it is always clear on what the final price will be, and there will be no surprises."},
+                
+            ]
+        }
+    }
+    render(){
+        return(
+            <div className="maincontainer" id="#details">
+                <p style={{fontSize:'40px', textAlign:'left'}}><strong>Details</strong></p>
+                <p style={{fontSize:'20px', textAlign:'left'}}>Creating a relationship of trust and a pleasant atmosphere is essential for every successful partnership. Therefore, following aspects are part of all of our projects:</p>
+                {this.state.data.map(d => 
+                    <Services data = {d}/>
+                )}
+            </div>
+        )
+    }
+}
+
+function Services(props){
+    return(
+        <div style={{minHeight:'100px'}}>
+                    <Grid container>
+                    <Grid item xs={12} md={3} className="gridContent" style={{padding:'5%'}}>
+                        <img src={props.data.img} alt = {props.data.name} style={{width:'40%'}}/>
+                    </Grid>
+                    <Grid item xs={12} md={9} className="gridContent">
+                    <div style={{textAlign:'justify'}}>
+                        <strong>{props.data.name}</strong><br/><br/>
+                        {props.data.description}
+                    </div>
+                    </Grid>
+                    </Grid>
+                </div>
+    )
+}
+export default DetailsComponent
--- src/Components/PortfolioComponent.jsx
+++ src/Components/PortfolioComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -18,60 +18,60 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ComplexGrid() {
+export default class PortfolioComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [
+        { id: 1, img1: Speedwell1, img2: Speedwell2, img3: Speedwell3, name: "Speedwell", description: "Cycle Industries", place: "Ahmedabad" },
+        { id: 2, img1: Speedwell1, img2: Speedwell2, img3: Speedwell3, name: "Nutan Vastra Bhandar", description: "Online Shopping", place: "Ahmedabad" },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ComplexGrid data={this.state.data} />
+      </div>
+    )
+  }
+}
+
+function ComplexGrid(props) {
   const classes = useStyles();
 
   return (
     <div id="#projects">
+      {/* eslint-disable-next-line */}
       <div className={classes.root} className="maincontainer">
         <p style={{ fontSize: '40px', textAlign: 'left' }}><strong>Portfolio</strong></p>
       </div>
-
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '10%' }}>
-            <h1>SPEEDWELL</h1>
-          </Grid>
-          <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '15%' }}>
-            <div className="trans">
-              <img alt="complex" src={Speedwell3} style={{ width: '45%', border: '1px solid' }} />
-            </div>
-            <div className="avatar">
-              <div className="trans">
-                <img alt="complex" src={Speedwell2} style={{ width: '40%', border: '1px solid', translate: '135% -150%' }} />
-              </div>
-            </div>
-            <div className="avatar">
-              <div className="trans">
-                <img alt="complex" src={Speedwell1} style={{ width: '40%', border: '1px solid', position: 'absolute', translate: '-2% -160%' }} />
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-      </Paper>
-
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '15%' }}>
-            <div className="trans">
-              <img alt="complex" src={Speedwell3} style={{ width: '45%', border: '1px solid' }} />
-            </div>
-            <div className="avatar">
-              <div className="trans">
-                <img alt="complex" src={Speedwell2} style={{ width: '40%', border: '1px solid', translate: '135% -150%' }} />
-              </div>
-            </div>
-            <div className="avatar">
-              <div className="trans">
-                <img alt="complex" src={Speedwell1} style={{ width: '40%', border: '1px solid', position: 'absolute', translate: '-2% -160%' }} />
-              </div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '10%' }}>
-            <h1>SPEEDWELL</h1>
-          </Grid>
-        </Grid>
-      </Paper>
+      {
+        props.data.map(data =>
+          <Paper className={classes.paper}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '10%' }}>
+                <h1>{data.name}</h1>
+              </Grid>
+              <Grid item xs={12} md={5} style={{ marginTop: '5%', paddingTop: '15%' }}>
+                <div className="trans">
+                  <img alt="complex" src={data.img3} style={{ width: '45%', border: '1px solid' }} />
+                </div>
+                <div className="avatar">
+                  <div className="trans">
+                    <img alt="complex" className="trans1" src={data.img2} style={{ width: '40%', border: '1px solid' }} />
+                  </div>
+                </div>
+                <div className="avatar">
+                  <div className="trans">
+                    <img alt="complex" className="trans2" src={data.img1} style={{ width: '40%', border: '1px solid', position: 'absolute' }} />
+                  </div>
+                </div>
+              </Grid>
+            </Grid>
+          </Paper>
+        )
+      }
     </div >
   );
 }
