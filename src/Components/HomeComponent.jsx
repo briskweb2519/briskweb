7f0bf9497d92408cc91968f463b8e6397e28d4e6
Shail <raval.shail1@gmail.com>
2020-07-05 14:40:46 +0530

    commit

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -10,7 +10,7 @@ class HomeComponent extends Component {
                 </div>
                 {/* <h1>BRISKWEB</h1>
                 <h2>We will take your business online</h2> */}
-                <div style={{padding : '5%'}}>
+                <div className="maincontainer" style={{padding : '5%'}}>
                 <h2> Briskweb is a Software Development Company to develop website and application for your business and help it expand with the tools of digital marketing</h2>
                 </div>
             </div>
